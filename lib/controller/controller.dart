f9f6c5a3dd5f06664ef1f13c7ef6b79f3eae3c73
samuel.mindler <samuel.mindler@estudiantes.utec.edu.uy>
2024-09-28 19:22:16 -0300

    norify listeners

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -43,6 +43,8 @@ Future<bool> isAdmin()async{
   Future<void> addProduct(Product product) async {
     try {
       await _dio.post('$_baseUrl/products', data: product.toJson());
+      //productos.add(product);
+      notifyListeners();
     } catch (e) {
       print('Error adding product: $e');
     }
@@ -51,6 +53,9 @@ Future<bool> isAdmin()async{
   Future<void> updateProduct(Product product) async {
     try {
       await _dio.put('$_baseUrl/products', data: product.toJson());
+      //final index = productos.indexWhere((element) => element.id == product.id);
+     // productos[index] = product;
+      notifyListeners();
     } catch (e) {
       print('Error updating product: $e');
     }
@@ -59,6 +64,8 @@ Future<bool> isAdmin()async{
   Future<void> deleteProduct(int id) async {
     try {
       await _dio.delete('$_baseUrl/products/$id');
+     //productos.removeWhere((element) => element.id == id);
+      notifyListeners();
     } catch (e) {
       print('Error deleting product: $e');
     }
@@ -102,6 +109,7 @@ Future<bool> isAdmin()async{
   Future<void> addToCart(CartItem item) async {
     try {
       await _dio.post('$_baseUrl/cart', data: item.toJson());
+      notifyListeners();
     } catch (e) {
       print('Error adding item to cart: $e');
     }
@@ -110,6 +118,7 @@ Future<bool> isAdmin()async{
   Future<void> updateCartItem(CartItem item) async {
     try {
       await _dio.put('$_baseUrl/cart/${item.product.name}', data: item.toJson());
+      notifyListeners();
     } catch (e) {
       print('Error updating cart item: $e');
     }
@@ -118,6 +127,7 @@ Future<bool> isAdmin()async{
   Future<void> removeFromCart(CartItem item) async {
     try {
       await _dio.post('$_baseUrl/cart/${item.product.id}/remove', data:{item.quantity});
+      notifyListeners();
     } catch (e) {
       print('Error removing item from cart: $e');
     }
@@ -142,203 +152,6 @@ Future<bool> isAdmin()async{
     }
   }
   Future<void> buyCart()async{
-
+    
   }
-  /* EMPIEZA EL MOCK
-
-  Future<bool> isAdmin() async {
-    return admin;
-  }
-  String randomImageApi =   "https://random.imagecdn.app/500/500";
-  List<Product> productos = [
-    Product(
-        id: 1,
-        description: "prod1",
-        image:
-            "https://random.imagecdn.app/500/500",
-        name: "prod1",
-        price: 12.5,
-        stock: 10),
-    Product(
-        id: 2,
-        description: "prod2",
-        image:
-            "https://random.imagecdn.app/500/500",
-        name: "prod2",
-        price: 12.5,
-        stock: 12),
-    Product(
-        id: 3,
-        description: "prod3",
-        image:
-            "https://random.imagecdn.app/500/500",
-        name: "prod3",
-        price: 12.5,
-        stock: 13),
-    Product(
-        id: 4,
-        description: "prod4",
-        image:
-            "https://random.imagecdn.app/500/500",
-        name: "prod4",
-        price: 12.5,
-        stock: 14)
-  ];
-
-  List<CartItem> cartItemsMock = [
-    CartItem(
-        product: Product(
-            id: 1,
-            description: "prod1",
-            image:
-                "https://random.imagecdn.app/500/500",
-            name: "prod1",
-            price: 12.5,
-            stock: 10),
-        quantity: 1),
-    CartItem(
-        product: Product(
-            id: 4,
-            description: "prod4",
-            image:
-                "https://random.imagecdn.app/500/500",
-            name: "prod4",
-            price: 12.5,
-            stock: 14),
-        quantity: 5),
-  ];
-
-  List<Order> orders = [
-    Order(id: 0, clientId: 0, clientName: "clientName", date: "date", quantity: 1, totalAmount: 123, products: [
-      CartItem(
-          product: Product(
-              id: 1,
-              description: "prod1",
-              image:
-                  "https://random.imagecdn.app/500/500",
-              name: "prod1",
-              price: 12.5,
-              stock: 10),
-          quantity: 1),
-      CartItem(
-          product: Product(
-              id: 4,
-              description: "prod4",
-              image:
-                  "https://random.imagecdn.app/500/500",
-              name: "prod4",
-              price: 12.5,
-              stock: 14),
-          quantity: 5),
-    ]),
-  ];
-
-  // Productos
-  Future<List<Product>> getProducts() async {
-    return productos;
-  }
-
-  Future<Product?> getProduct(int id) async {
-    return productos.firstWhere((element) => element.id == id);
-  }
-
-  Future<void> addProduct(Product product) async {
-    product.id = productos.length + 1;
-    print("nuevo producto: ${product.id}");
-
-    productos.add(product);
-    productos.forEach((Product p) {
-      print(p.name);
-    });
-    notifyListeners();
-  }
-
-  Future<void> updateProduct(Product product) async {
-    final index = productos.indexWhere((element) => element.id == product.id);
-    productos[index] = product;
-    notifyListeners();
-  }
-
-  Future<void> deleteProduct(int id) async {
-    productos.removeWhere((element) => element.id == id);
-    notifyListeners();
-  }
-
-  // Órdenes
-  Future<List<Order>> getUserOrders() async {
-    return orders.toList();
-  }
-
-  Future <List<OrderItem>> getProductOrders(int productId) async {
-    Product ? product = await getProduct(productId);
-    if(product == null){
-      return [];
-    }
-
-    List<OrderItem> orderItems = [OrderItem(id:0, orderId: 0 ,product: product, quantity: 1, purchaseUnitPrice: 10.5)];
-
-    return orderItems;
-  }
-  
-  Future<Order?> getOrderDetails(int orderId) async {
-    return orders.firstWhere((element) => element.id == orderId);
-  }
-
-  // Carrito
-  Future<List<CartItem>> getCartItems() async {
-    return cartItemsMock;
-  }
-
-  Future<void> addToCart(CartItem item) async {
-    cartItemsMock.add(item);
-    print('Adding to cart: ${item.product.name}');
-    notifyListeners();
-  }
-
-  Future<void> updateCartItem(CartItem item) async {
-    cartItemsMock[cartItemsMock
-        .indexWhere((element) => element.product.name == item.product.name)] = item;
-    print('Updating cart item: ${item.product.name}');
-    //notifyListeners();
-  }
-
-  Future<void> removeFromCart(CartItem item) async {
-    cartItemsMock.removeWhere((element) => element.product.id == item.product.id);
-    notifyListeners();
-  }
-
-  Future<void> buyCart() async{
-    var insertID = orders.length;
-    var nuevaOrden = Order(
-        id: insertID,
-        clientId: loggedUser.id,
-        clientName: loggedUser.email,
-        date: DateTime.now().toString(),
-        quantity: 2,
-        totalAmount: 24.00,
-        products: cartItemsMock);
-    cartItemsMock = [];
-    orders.add(nuevaOrden);
-    notifyListeners();
-  }
-
-
-
-  // Autenticación
-  Future<void> register(String email, String password) async {
-    // TODO implementar
-  }
-
-  Future<LoginResponse> login(String email, String password) async {
-    loggedUser.email = email;
-    if (email == 'admin') {
-      admin = true;
-      return LoginResponse(logged: true, token: ' token', isAdmin: true, user: loggedUser);
-    } if( email =="error"){
-      return LoginResponse(logged: false, token: '', isAdmin: false);
-    } else {
-      admin = false;
-      return LoginResponse(logged: true, token: 'token', isAdmin: false, user: loggedUser);
-    }
-  }*/
 }
